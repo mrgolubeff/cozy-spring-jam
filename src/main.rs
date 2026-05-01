95420732c70fa037bebd3cddd3b1076c82b54888
Vladimir Golubev <inbox@golubevs.art>
2026-05-02 00:09:26 +0300

    Create a main window sample

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use bevy::prelude::*;
+
+fn main() {
+    let default_plugins = DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Cozy Spring Jam".into(),
+            resolution: (800, 600).into(),
+            ..default()
+        }),
+        ..default()
+    });
+
+    App::new().add_plugins(default_plugins).run();
+}
