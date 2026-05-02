8a1722305e7def1a4bb4679124a8361162845eb4
Vladimir Golubev <inbox@golubevs.art>
2026-05-02 10:09:24 +0300

    Change window resolution and add a comment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
 
 fn main() {
+    // Set up DefaultPlugins to render window.
     let default_plugins = DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             title: "Cozy Spring Jam".into(),
-            resolution: (800, 600).into(),
+            resolution: (1280, 720).into(),
             ..default()
         }),
         ..default()
